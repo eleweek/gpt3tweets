4a3cad21dab59ba8d94b9b4ef3abe9fb8b25884a
Alexander Putilin <avp-13@yandex.ru>
2023-02-04 16:57:50 +0000

    Slightly better cli interface

--- scripts/scrapeTweets.js
+++ scripts/scrapeTweets.js
@@ -4,7 +4,8 @@ import { argv } from 'node:process';
 
 const FAKE_USER_AGENT_STRING = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36";
 
-export default async function getData(url) {
+export default async function getTweets(user) {
+  const url = `https://twitter.com/search?q=from%3A${user}&src=typed_query`
   const options = process.env.AWS_REGION
     ? {
         args: chrome.args,
@@ -20,23 +21,29 @@ export default async function getData(url) {
             ? '/usr/bin/google-chrome'
             : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
       };
-  console.log("Creating browser...");
+  
+      console.log("Creating browser...");
   const browser = await puppeteer.launch(options);
+
   console.log("Awaiting newPage...");
   const page = await browser.newPage();
   await page.setUserAgent(
     FAKE_USER_AGENT_STRING
   )
+
   console.log("Setting viewport size...");
   await page.setViewport({ width: 2000, height: 1000 });
+  
   console.log("Going to", url);
   await page.goto(url, { waitUntil: 'networkidle0' });
+  
   console.log("Evaluating selector for the html...");
   const results = await page.$$eval('article div[lang]', (tweets) => tweets.map((tweet) => tweet.textContent));
+  
   console.log(results);
 
   return results;
 }
 
 console.log(argv);
-getData(argv[2]).then(console.log).catch(e => console.error(e));
+getTweets(argv[2]).then(console.log).catch(e => console.error(e));

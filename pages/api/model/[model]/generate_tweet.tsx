9bbefd4aeb50770480e12a1fea182abfb793de17
Alexander Putilin <avp-13@yandex.ru>
2023-02-07 18:10:05 +0000

    Generate AI tweets

--- pages/api/model/[model]/generate_tweet.tsx
+++ pages/api/model/[model]/generate_tweet.tsx
@@ -3,18 +3,55 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+import { createClient } from "@supabase/supabase-js";
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
+);
+
 export default async function handler(req, res) {
   const { model } = req.query;
 
+  const { data, error } = await supabase
+    .from("models")
+    .select("openai_model_name, tweet_prompt")
+    .eq("display_model_name", model);
+
   console.log("generate_tweet handler", model);
 
-  const openai = new OpenAIApi(configuration);
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: "Say this is a test",
-    max_tokens: 7,
-    temperature: 0,
-  });
+  if (data) {
+    if (data.length === 0) {
+      res.end(
+        JSON.stringify({
+          error: "No model found",
+          text: null,
+        })
+      );
+    }
 
-  res.end(JSON.stringify({ text: response.data.choices[0].text }));
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createCompletion({
+      model: data[0].openai_model_name,
+      prompt: data[0].tweet_prompt.endsWith("\n")
+        ? data[0].tweet_prompt
+        : data[0].tweet_prompt + "\n",
+      max_tokens: 64,
+      temperature: 0.9,
+      stop: ["###", "######"],
+    });
+
+    res.end(
+      JSON.stringify({
+        error: null,
+        text: response.data.choices[0].text,
+      })
+    );
+  } else {
+    res.end(
+      JSON.stringify({
+        error: error,
+        text: null,
+      })
+    );
+  }
 }
--- pages/model/[name].tsx
+++ pages/model/[name].tsx
@@ -1,16 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 
 import Head from "next/head";
+
+async function generateTweet(name) {
+  const res = await fetch(`/api/model/${name}/generate_tweet`);
+  const json = await res.json();
+  console.log("generateTweet", json);
+
+  return json;
+}
+
+function TweetSpinner() {
+  return (
+    <svg
+      className="animate-spin h-10 w-10 text-indigo-500"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        className="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        stroke-width="4"
+      ></circle>
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
+  );
+}
+
+function Tweet({ text, isLoading }) {
+  return (
+    <div className="p-6 rounded-2xl bg-white">
+      {isLoading ? <TweetSpinner /> : <p className="text-3xl">{text}</p>}
+    </div>
+  );
+}
 
 export default function Model() {
   const { query, isReady } = useRouter();
   const { name } = query;
 
+  const [tweet, setTweet] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  console.log("Router", query, isReady);
   if (isReady) {
-    fetch(`/api/model/${name}/generate_tweet`)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    console.log("Generating tweet for", name);
+    if (!tweet && !error && !isLoading) {
+      setIsLoading(true);
+      generateTweet(name).then((json) => {
+        console.log("generateTweet", json);
+        if (json.error) {
+          setError(json.error);
+          setIsLoading(false);
+        } else {
+          setTweet(json.text);
+          setIsLoading(false);
+        }
+      });
+    }
   }
 
   return (
@@ -20,7 +77,8 @@ export default function Model() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="px-16 py-24">
-        <h1 className="text-5xl font-bold py-6">GPT-3 tweets for {name}</h1>
+        <h1 className="text-5xl font-bold py-6">{name}: GPT-3 tweets</h1>
+        <Tweet text={tweet} isLoading={isLoading} />
       </div>
     </div>
   );
